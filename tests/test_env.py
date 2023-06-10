59cec713d59d9147f79b580cef88956af3a88821
Mikihisa Yuasa <myuasa2@illinois.edu>
2023-06-10 00:54:23 -0500

    fix: correct the used env

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,13 +1,13 @@
-from gym_multigrid.envs.collect_game import CollectGameEnv
+from gym_multigrid.envs.collect_game import CollectGame3Obj2Agent
 
 
 def test_collect_game() -> None:
     """Test collect_game()"""
-    env = CollectGameEnv()
+    env = CollectGame3Obj2Agent()
 
     obs, _ = env.reset()
     while True:
         action = env.action_space.sample()
-        obs, reward, terminated, truncated, info = env.step(action)
+        obs, reward, terminated, truncated, info = env.step([action])
         if terminated or truncated:
             break
