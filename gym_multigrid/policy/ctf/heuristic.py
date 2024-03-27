077efe7e4b1c75895cea00385856719cd8bf8485
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-03-27 14:27:01 -0500

    fix: add reproducible random generator

--- gym_multigrid/policy/ctf/heuristic.py
+++ gym_multigrid/policy/ctf/heuristic.py
@@ -1,6 +1,7 @@
 from typing import TypedDict
 
 import numpy as np
+from numpy.random import Generator
 
 from gym_multigrid.core.agent import ActionsT
 from gym_multigrid.policy.base import BaseAgentPolicy, ObservationT
@@ -9,11 +10,18 @@ from gym_multigrid.policy.base import BaseAgentPolicy, ObservationT
 class RwPolicy(BaseAgentPolicy):
     """
     Random walk policy
+
+    Attributes:
+        name: str
+            Policy name
+        random_generator: numpy.random.Generator
+            Random number generator. Replace it with the environment's random number generator if needed.
     """
 
     def __init__(self) -> None:
         super().__init__()
         self.name = "rw"
+        self.random_generator: Generator = np.random.default_rng()
 
     def act(self, observation: ObservationT, actions: ActionsT) -> int:
-        return np.random.choice(list(actions))
+        return self.random_generator.integers(0, len(actions))
