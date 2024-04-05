b58a6834b66c044356061d91ffeecef40857388c
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-04-05 08:54:33 -0500

    cho: add ignored dirs

---
+++ scripts/main_mvn_ctf_rl.py
@@ -0,0 +1,44 @@
+from stable_baselines3 import PPO
+import torch
+import imageio
+
+from gym_multigrid.envs.ctf import CtFMvNEnv
+
+total_timesteps: int = 1_000_000
+tb_log_dir: str = "out/logs/"
+model_save_path: str = "out/models/ctf_ppo"
+# Create the environment
+map_path: str = "tests/assets/board.txt"
+env = CtFMvNEnv(
+    num_blue_agents=2,
+    num_red_agents=2,
+    map_path=map_path,
+    render_mode="rgb_array",
+    observation_option="flattened",
+)
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Create the RL model
+model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tb_log_dir)
+
+# Train the model
+model.learn(total_timesteps=total_timesteps)
+
+# Save the model
+model.save(model_save_path)
+
+# Save an animation
+obs, _ = env.reset()
+
+imgs = [env.render()]
+
+while True:
+    actions = model.predict(obs)
+    obs, reward, terminated, truncated, info = env.step(actions)
+    imgs.append(env.render())
+
+    if terminated or truncated:
+        break
+
+imageio.mimsave("out/animations/ctf_ppo.gif", imgs, fps=0.5)
