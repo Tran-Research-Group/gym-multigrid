dbe9c604fd96ad853c913bcf44f6db9dba4b0d43
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-04-05 09:14:24 -0500

    feat: add mvn ctf test RL script

--- scripts/main_mvn_ctf_rl.py
+++ scripts/main_mvn_ctf_rl.py
@@ -5,7 +5,8 @@ import imageio
 from gym_multigrid.envs.ctf import CtFMvNEnv
 
 total_timesteps: int = 1_000_000
-tb_log_dir: str = "out/logs/"
+tb_log_dir: str = "out/logs/ctf/"
+tb_log_name: str = "ctf_mvn_ppo"
 model_save_path: str = "out/models/ctf_ppo"
 # Create the environment
 map_path: str = "tests/assets/board.txt"
@@ -23,7 +24,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tb_log_dir)
 
 # Train the model
-model.learn(total_timesteps=total_timesteps)
+model.learn(total_timesteps=total_timesteps, tb_log_name=tb_log_name)
 
 # Save the model
 model.save(model_save_path)
