ae6f1cbad3ec83724908e12c010345658fec4e39
Minjae Cho <whalswo117@gmail.com>
2024-08-01 17:01:09 -0500

    bug fixed and test file is uploaded

---
+++ tests/test_fourroom.py
@@ -0,0 +1,33 @@
+import pytest
+import os
+
+import numpy as np
+import imageio
+from stable_baselines3 import PPO
+
+from gym_multigrid.envs.fourrooms import FourRooms
+from gym_multigrid.policy.ctf.heuristic import (
+    FightPolicy,
+    CapturePolicy,
+    PatrolPolicy,
+    RwPolicy,
+    PatrolFightPolicy,
+)
+from gym_multigrid.utils.map import load_text_map
+import matplotlib.pyplot as plt
+
+
+def test_fr() -> None:
+    env = FourRooms(render_mode="human")
+    obs, _ = env.reset()
+    env.render()
+
+    while True:
+        action = np.random.choice(list(env.actions_set))
+        obs, reward, terminated, truncated, info = env.step(action)
+        env.render()
+        if terminated or truncated:
+            break
+
+
+test_fr()
