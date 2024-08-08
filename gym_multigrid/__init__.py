feae08004c57c70838474e352080cb69b6d108c7
apurl1 <rupalnigam17@gmail.com>
2024-08-07 22:03:14 -0500

    update gym register calls to use max episode steps

--- gym_multigrid/__init__.py
+++ gym_multigrid/__init__.py
@@ -6,31 +6,105 @@ from gymnasium.envs.registration import register
 register(
     id="multigrid-collect-v0",
     entry_point="gym_multigrid.envs:CollectGame3Obj2Agent",
+    max_episode_steps=100,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
 )
-# Collect game with 2 agents and 3 object types with fixed locations
-# ----------------------------------------
-register(
-    id="multigrid-collect-fixed-v0",
-    entry_point="gym_multigrid.envs:CollectGame3ObjFixed2Agent",
-)
-# Collect game with 1 agent and 3 object types
+# Collect game with single agent and 3 object types
 # ----------------------------------------
 register(
     id="multigrid-collect-single-v0",
-    entry_point="gym_multigrid.envs:CollectGame3ObjSingleAgent",
+    entry_point="gym_multigrid.envs:CollectGame3Obj2Agent",
+    max_episode_steps=100,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3],  # green
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
 )
-
+# Collect game with 2 agents and 3 object types clustered in different quadrants of the grid
+# ----------------------------------------
+register(
+    id="multigrid-collect-quadrants-v0",
+    entry_point="gym_multigrid.envs:CollectGameQuadrants",
+    max_episode_steps=100,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
+)
+# Collect game with 2 agents and 3 object types clustered differently in four rooms
+# ----------------------------------------
 register(
     id="multigrid-collect-rooms-v0",
     entry_point="gym_multigrid.envs:CollectGameRooms",
+    max_episode_steps=100,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
 )
 
 register(
     id="multigrid-collect-rooms-fixed-horizon-v0",
     entry_point="gym_multigrid.envs:CollectGameRoomsFixedHorizon",
+    max_episode_steps=100,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
 )
 
 register(
     id="multigrid-collect-rooms-respawn-v0",
     entry_point="gym_multigrid.envs:CollectGameRoomsRespawn",
+    max_episode_steps=50,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
+)
+
+register(
+    id="multigrid-collect-respawn-v0",
+    entry_point="gym_multigrid.envs:CollectGameRespawn",
+    max_episode_steps=50,
+    kwargs={
+        "size": 10,
+        "num_balls": 15,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
+)
+
+register(
+    id="multigrid-collect-quadrants15-v0",
+    entry_point="gym_multigrid.envs:CollectGameQuadrants",
+    kwargs={
+        "size": 15,
+        "num_balls": 30,
+        "agents_index": [3, 5],  # green, purple
+        "balls_index": [0, 1, 2],  # red, orange, yellow
+        "balls_reward": [1, 1, 1],
+    },
 )
