e8b80e8fc029d88c903441decb53683b7485de66
Mikihisa Yuasa <myuasa2@illinois.edu>
2024-10-06 16:53:43 -0500

    feat: add 1v1 ctf example script

---
+++ scripts/main_1v1_ctf_rl.py
@@ -0,0 +1,50 @@
+import os
+
+from stable_baselines3 import PPO
+import torch
+import imageio
+
+from gym_multigrid.envs.ctf import Ctf1v1Env
+
+total_timesteps: int = 1_000_000
+tb_log_dir: str = "out/logs/ctf/"
+tb_log_name: str = "ctf_1v1_ppo"
+model_save_path: str = "out/models/ctf_ppo_1v1"
+# Create the environment
+map_path: str = "tests/assets/board_wall.txt"
+env = Ctf1v1Env(
+    map_path=map_path,
+    render_mode="rgb_array",
+    observation_option="pos_map_flattened",
+)
+
+print("GPU available: ", torch.cuda.is_available())
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+if os.path.exists(model_save_path + ".zip"):
+    model = PPO.load(model_save_path, env=env, device=device)
+else:
+    # Create the RL model
+    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tb_log_dir)
+
+    # Train the model
+    model.learn(total_timesteps=total_timesteps, tb_log_name=tb_log_name)
+
+    # Save the model
+    model.save(model_save_path)
+
+# Save an animation
+obs, _ = env.reset()
+
+imgs = [env.render()]
+
+while True:
+    actions, _ = model.predict(obs)
+    obs, reward, terminated, truncated, info = env.step(actions)
+    imgs.append(env.render())
+
+    if terminated or truncated:
+        break
+
+imageio.mimsave("out/animations/ctf_1v1_ppo.gif", imgs, fps=5)
