c8be03b2a4b29a51e00b2c625b1ba87a634e1f85
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-02-09 14:10:29 -0600

    test: add step test

--- tests/test_preypred.py
+++ tests/test_preypred.py
@@ -12,6 +12,25 @@ def test_preypred_init() -> None:
     assert env is not None
 
 
+def test_preypred_step() -> None:
+    """Test PreyPredEnv step"""
+    animation_path = "tests/out/animations/test_preypred_step.gif"
+    env = gym.make("multigrid-preypred-v0")
+    env.reset()
+    frames = [env.render()]
+
+    while True:
+        actions = [env.action_space.sample() for _ in range(3)]
+        obs, reward, terminated, truncated, info = env.step(actions)
+        frames.append(env.render())
+        if terminated or truncated:
+            break
+
+    os.makedirs(os.path.dirname(animation_path), exist_ok=True)
+    imageio.mimsave(animation_path, frames, duration=0.5, loop=20)
+    assert os.path.exists(animation_path)
+
+
 def test_preypred_render() -> None:
     """Test PreyPredEnv render"""
     image_path = "tests/out/plots/test_preypred_render.png"
