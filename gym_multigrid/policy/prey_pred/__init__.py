70aebc53564bfbf15e309dcfbc5033fff2e42931
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-02-08 20:49:00 -0600

    feat: add given policy to take step action

--- gym_multigrid/policy/prey_pred/__init__.py
+++ gym_multigrid/policy/prey_pred/__init__.py
@@ -1,10 +1,23 @@
 import enum
-from typing import Type
+from typing import Any, Type
 
 from gymnasium.core import ObsType
 import numpy as np
 
 from gym_multigrid.policy.base import BaseAgentPolicy, AgentPolicyT
+
+
+class GivenPolicy(BaseAgentPolicy[ObsType, int]):
+    def __init__(
+        self,
+        action_set: Type[enum.IntEnum] | None = None,
+        random_generator: np.random.Generator | None = None,
+    ):
+        super().__init__(action_set, random_generator)
+        self.name = "given"
+
+    def act(self, observation: ObsType, options: dict[str, Any]):
+        return options["action"]
 
 
 class RwPolicy(BaseAgentPolicy[ObsType, int]):
@@ -16,7 +29,7 @@ class RwPolicy(BaseAgentPolicy[ObsType, int]):
         super().__init__(action_set, random_generator)
         self.name = "random"
 
-    def act(self, observation):
+    def act(self, observation: ObsType, options: dict[str, Any]) -> int:
         return self.random_generator.choice(self.action_set)
 
 
