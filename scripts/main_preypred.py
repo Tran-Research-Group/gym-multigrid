b303753a8d4203fcbc3c66000a882d100cf1077d
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-02-09 15:02:57 -0600

    feat: update docs

---
+++ scripts/main_preypred.py
@@ -0,0 +1,26 @@
+import os
+import pprint
+
+import gymnasium as gym
+import imageio
+
+import gym_multigrid
+
+animation_path = "out/animations/test_preypred_step.gif"
+
+env = gym.make("multigrid-preypred-v0")
+
+obs, _ = env.reset()
+frames = [env.render()]
+
+while True:
+    actions = [env.action_space.sample() for _ in range(3)]
+    obs, reward, terminated, truncated, _ = env.step(actions)
+    print(f"Reward: {reward}")
+    pprint.pprint(obs)
+    frames.append(env.render())
+    if terminated or truncated:
+        break
+
+os.makedirs(os.path.dirname(animation_path), exist_ok=True)
+imageio.mimsave(animation_path, frames, duration=2, loop=20)
