02b318d01a21a8dfa48a0458dec4e089561e9503
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-02-10 22:27:03 -0600

    feat: update the example

--- scripts/main_preypred.py
+++ scripts/main_preypred.py
@@ -5,16 +5,43 @@ import gymnasium as gym
 import imageio
 
 import gym_multigrid
+from gym_multigrid.envs.prey_pred import (
+    GreedyPredatorPolicy,
+    GreedyPredatorActionOption,
+)
 
 animation_path = "out/animations/test_preypred_step.gif"
-animation_path_2 = "out/animations/test_preypred_step_2.gif"
 
 env = gym.make("multigrid-preypred-v0")
 
 obs, _ = env.reset()
 frames = [env.render()]
 
+# Initialize greedy policies for heuristic predators
+greedy_policy_1 = GreedyPredatorPolicy([1, 1, 1, 0], random_generator=env.np_random)
+greedy_policy_2 = GreedyPredatorPolicy([0, 0, 1, 1], random_generator=env.np_random)
+
 while True:
+    # Create inputs for the greedy policies
+    prey_agents = env.unwrapped.prey_agents
+    grid = env.unwrapped.grid
+    agent_1_option: GreedyPredatorActionOption = {
+        "current_pos": env.unwrapped.agents[1].pos,
+        "preys": prey_agents,
+        "grid": grid,
+    }
+    agent_2_option: GreedyPredatorActionOption = {
+        "current_pos": env.unwrapped.agents[2].pos,
+        "preys": prey_agents,
+        "grid": grid,
+    }
+
+    # Get actions from the greedy policies
+    # Agent 0 is a random agent
+    actions = [env.action_space.sample()] + [
+        greedy_policy_1.act(agent_1_option),
+        greedy_policy_2.act(agent_2_option),
+    ]
     actions = [env.action_space.sample() for _ in range(3)]
     obs, reward, terminated, truncated, _ = env.step(actions)
     print(f"Reward: {reward}")
@@ -26,17 +53,17 @@ while True:
 os.makedirs(os.path.dirname(animation_path), exist_ok=True)
 imageio.mimsave(animation_path, frames, duration=2, loop=20)
 
-obs, _ = env.reset()
-frames = [env.render()]
+# obs, _ = env.reset()
+# frames = [env.render()]
 
-while True:
-    actions = [env.action_space.sample() for _ in range(3)]
-    obs, reward, terminated, truncated, _ = env.step(actions)
-    print(f"Reward: {reward}")
-    pprint.pprint(obs)
-    frames.append(env.render())
-    if terminated or truncated:
-        break
+# while True:
+#     actions = [env.action_space.sample() for _ in range(3)]
+#     obs, reward, terminated, truncated, _ = env.step(actions)
+#     print(f"Reward: {reward}")
+#     pprint.pprint(obs)
+#     frames.append(env.render())
+#     if terminated or truncated:
+#         break
 
-os.makedirs(os.path.dirname(animation_path_2), exist_ok=True)
-imageio.mimsave(animation_path_2, frames, duration=2, loop=20)
+# os.makedirs(os.path.dirname(animation_path_2), exist_ok=True)
+# imageio.mimsave(animation_path_2, frames, duration=2, loop=20)
