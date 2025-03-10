9af47da03396433a2584657e1edd72de4ef92396
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-03-10 18:44:32 -0500

    test: add more coverage

--- tests/test_maze.py
+++ tests/test_maze.py
@@ -7,7 +7,7 @@ import gymnasium as gym
 import imageio
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from gym_multigrid.envs.maze import MazeEnv
+from gym_multigrid.envs.maze import LayoutConfig, MazeEnv
 
 
 def test_maze_run() -> None:
@@ -27,3 +27,29 @@ def test_maze_run() -> None:
 
     imageio.mimsave(animation_save_path, images, duration=0.5)
     assert os.path.exists(animation_save_path)
+
+
+def test_maze_reset() -> None:
+    env = gym.make("multigrid-maze-v0")
+    obs, _ = env.reset()
+    assert obs.shape == (2, 10, 10)
+    assert obs[0, 9, 9] == 2
+    assert obs[1, 5, 5] == 1
+
+
+def test_maze_reset_options() -> None:
+    layout_config = {
+        "init_agent_positions": [(3, 6)],
+        "flag_positions": [(7, 9)],
+    }
+    options = {
+        "layout_config": layout_config,
+    }
+    env = gym.make("multigrid-maze-v0")
+
+    obs, _ = env.reset(options=options)
+    assert obs.shape == (2, 10, 10)
+    assert obs[0, 7, 9] == 2
+    assert obs[1, 3, 6] == 1
+    assert obs[0, 9, 9] == 0
+    assert obs[1, 5, 5] == 0
