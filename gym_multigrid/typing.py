e88850e21219145aeb93de214e0a27f35ab9f327
Mikihisa Yuasa <myuasa2@illinois.edu>
2025-07-21 14:17:43 -0500

    feat: add gym.make Pydantic schema

--- gym_multigrid/typing.py
+++ gym_multigrid/typing.py
@@ -1,4 +1,18 @@
-from typing import TypeAlias
+from typing import Generic, TypeAlias, TypeVar
+
+from pydantic import BaseModel, ConfigDict
 
 Position: TypeAlias = tuple[int, int]
 Size: TypeAlias = tuple[int, int]
+
+EnvKwargs = TypeVar("EnvKwargs")
+
+
+class EnvMakeConfig(BaseModel, Generic[EnvKwargs]):
+    id: str = "multigrid-rooms-v0"
+    max_episode_steps: int | None
+    disable_env_checker: bool | None = None
+    env_kwargs: EnvKwargs
+
+    # allow arbitrary kwargs
+    model_config = ConfigDict(arbitrary_types_allowed=True)
